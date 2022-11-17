b7f5a3fb29d369b37fb466e20d791222ddb09ab8
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-11-17 11:56:58 +0500

    finished homework#36, DOM

--- HM36.js
+++ HM36.js
@@ -1,1 +1,109 @@
-console.log("hello")
+
+
+
+/*function red(){
+
+    document.body.style.background = 'red';
+    setTimeout(() => document.body.style.background = '', 2000);
+}
+//red();
+
+const body = document.body
+body.append('Hello World');
+
+const div = document.createElement('div');
+body.append(div);
+const div1 = document.createElement('div');
+body.append(div1);
+div.innerText = 'Hey, how are you doing, today?';
+div1.textContent = 'Yeah, I am good. And you?';
+
+div.innerHTML = "<strong> Hello World</strong>"
+
+for (let i = 0; i < document.body.childNodes.length; i++) {
+    console.log( document.body.childNodes[i] ); // Text, DIV, Text, UL, ..., SCRIPT
+  }
+
+  console.log( Array.from(document.body.childNodes).filter );
+
+  
+  let td = table.rows[0].cells[1];
+  td.style.backgroundColor = 'red';*/
+
+
+  //Tasks
+
+
+  //DOM children
+
+  //The <div> DOM node
+  
+  //console.log(document.body.firstElementChild);
+
+  //The <ul> DOM node 
+
+  //console.log(document.body.lastElementChild);
+
+  //The second <li> (with Pete)
+
+  //console.log(document.body.lastElementChild.lastElementChild);
+
+//let elem = document.getElementById('elem');
+//elem.style.background = 'red';
+
+/*let elem = document.querySelector('div');
+console.log(elem);*/
+
+/*console.dir(document.body.children);
+
+console.log(document.body.innerHTML);
+document.body.innerHTML = "The new BODY!";*/
+
+/*document.body.myData = {
+  name: "Caesar",
+  title: "Imperator"
+};
+
+console.log(document.body.myData.name);
+console.dir(document.body.myData);
+
+document.body.sayTagName = function(){
+  console.log(this.tagName);
+}
+document.body.sayTagName()*/
+
+//console.log(elem.getAttribute('About'));
+//elem.setAttribute('Test',123);
+//console.log(elem.outerHTML);
+
+/*let input = document.querySelector('input');
+input.setAttribute('id','id');
+console.log(input.id);
+
+input.id = 'newId';
+console.log(input.getAttribute('id'));*/
+
+
+/*let input = document.querySelector('input');
+
+
+input.setAttribute('value', 'text');
+console.log(input.value); 
+
+
+input.value = 'newValue';
+console.log(input.getAttribute('value'));
+
+  console.log(div.getAttribute('style')); 
+
+  
+  console.log(div.style); 
+  console.log(div.style.color); */
+
+ /* document.body.classList.add('article');
+  console.log(document.body.className);
+  document.body.classList.remove('main page article');
+  console.log(document.body.className);*/
+
+
+
