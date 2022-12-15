26a22cc6584b8e19fa701a804f79de420e658c59
Aziz Zulfikorov <tokzor123@yahoo.com>
2022-12-15 12:18:18 +0500

    still doing tasks on cookies.

--- Cookie.js
+++ Cookie.js
@@ -1,1 +1,9 @@
-console.log('Hello, world');
+
+
+function colorField(){
+    const color = document.getElementById("color").value;
+    let regexp = /[0-9]/g;
+    if(color.match(regexp)){
+        alert('This field should not include numbers!');
+    }
+}
